59003cecadeb6af376587eaa595744abc7fed949
deadpool9652 <darkgamer9652@gmail.com>
2026-06-17 03:40:26 +0530

    upload

---
+++ 4x1mux_using_2x1/4x1mux_using_2x1.srcs/sources_1/new/4x1mux.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.06.2026 14:38:26
+// Design Name: 
+// Module Name: 4x1mux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module 4x1mux(
+
+    );
+endmodule
